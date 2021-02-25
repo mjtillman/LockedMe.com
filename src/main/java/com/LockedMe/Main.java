2ea20e9498cb95385dd325865b516d0002222ef1
mjtillman <mjtillman@gmail.com>
2021-02-25 10:58:32 -0600

    First commit

---
+++ src/main/java/com/LockedMe/Main.java
@@ -0,0 +1,10 @@
+package com.LockedMe;
+
+public class Main {
+
+  public static void main(String[] args) {
+    MenuDisplay.welcome();
+    MenuDisplay.mainMenu();
+  }
+
+}
---
+++ src/main/java/com/LockedMe/Navigator.java
@@ -0,0 +1,5 @@
+package com.LockedMe;
+
+public class Navigator {
+
+}
